3d657cb1c7de9f40d5b77348480760360e310ea9
rupsabasu2020 <rupsa.basu2020@gmail.com>
2023-11-29 15:18:39 +0100

    new rescaled

--- basefuncs_multCPP.py
+++ basefuncs_multCPP.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import fMA_1_base as fMA1
+import matplotlib.pyplot as plt
 
 # ---- FINAL VERSION OF REQUIRED BASE FUNCTIONS ----------------#
 
@@ -9,95 +10,108 @@ import fMA_1_base as fMA1
 #--                           Initial binary segmentation                                         --#
 #---------------------------------------------------------------------------------------------------#
 
-def U_hat(s, fdata, l, r, N):
-    """  
-    statistic to initially segment data
-    Parameters:
-        --
-        --
-        --
-        --
 
+def U_hat(s, fdata, l, r, N):
+    """ CUSUM statistic to recover change points
+    
+    Parameters: 
+        -- s: s in (l, r) and the time point under consideration
+        --fdata: functional data in matrix form with dimensions (N, 100)   # 100 can be any other value
+        -- l: start of the sequence in (1, N)
+        -- r: end of the sequence in (1, N)
+        -- N: sample size of the full sequence
     """
     res = (1/(r-l)) * (np.sum(fdata[ l :(int(s * N)), :], axis=0) - ((int(s * N) - l) / (r - l)) * np.sum(fdata[l:r-1, :], axis=0))   # configure start to be prev. cp
     return res
 
+def cusum(fdata, begin, end, N, thresh_val):  #, thresh_val, cp_list,
+    """ iteratively using the CUSUM statistic above to recover locations of changes 
 
+    Parameters: 
+        --fdata:
+        --begin:
+        --end:
+        --N: 
+        --thresh_val:
 
-def cusum(data_mat, begin, end, N, thresh_val):  #, thresh_val, cp_list,
-    """ 
-    function to find multiple change locations using U_hat
-    Parameters : 
-        --
-        --
-        --
-        --  
     """
     
-    if begin >= end:
+    if begin >= end:    # defining new l and r of the sequence from recursive binseg
         return -1
     
     dataPoints = end - begin
     sValues = np.linspace(float(begin)/float(N), float(end - 1)/float(N), num=dataPoints)
     M_hat = []
     for i in range(dataPoints):  # scanning over possible values of s
-        U = U_hat(sValues[i], data_mat, begin, end, N)
-        #t_argmax = np.argmax(np.abs(U))  # sup over t     
-        hat_M =  np.max(np.abs(U))  #np.abs(U[t_argmax])  
-        M_hat.append(hat_M)
-    pos = -1
-    if np.max(M_hat) > thresh_val:   # binseg thresh for detecting
-
-        pos = np.argmax(M_hat)
+        U = U_hat(sValues[i], fdata, begin, end, N)
+        l2_normU = np.linalg.norm(U, ord=2)   # l2 norm
+        M_hat.append(l2_normU)
+    pos= -1
+    if np.max(M_hat)> thresh_val:
+        pos= np.argmax(M_hat)
+    if pos >0:
+        return pos + begin
+    else:
+        return pos
     
-    if pos > 0: 
-        return pos + begin, M_hat[pos] 
-    else: 
-        return pos, M_hat[pos] #sorted(cp_list)
-
-
-
-
-
-
 def binary_seg_init(data, thresh):
     """Binary segmentation for change-point detection."""
     N = len(data[:, 0])
     cp = [0, N]  # Initial interval
+    #cp2 = [0, N]
     return binary_seg_rec(data, thresh, cp, 0, N, N)
 
 
 def binary_seg_rec(data, thresh, cp, begin, end, N):
-    cp_location, _ = cusum(data, begin, end, N, thresh)
+    cp_location = cusum(data, begin, end, N, thresh)
     if cp_location > 0:
         cp.append(cp_location)
-        print(cp_location)
         cp.sort()
         cp = binary_seg_rec(data, thresh, cp, begin, cp_location - 1, N)
         cp = binary_seg_rec(data, thresh, cp, cp_location + 1, end, N)
     return cp
 
+
+
 #---------------------------------------------------------------------------------------------------#
 #-                           Step 2: Relevant change points                                        -#
 #---------------------------------------------------------------------------------------------------#
 
-def mult_test_stat(list_of_cp, fdata, binseg_thresh):
-    """
-    compute test statistic for each change point:
-        --
-        --
-        --
-        --
+
+
+
+
+def mult_test_stat(data_mat, N, cp_i):  #, thresh_val, cp_list,
+    """ Gets the test statistic of each change point. As this is in step 2, hat_M is the sup-norm.
+    Parameters:
+        -- : data_mat : functional data in dimension (N* 100)
+        -- begin : previous change location in (1, N)
+        -- end : next change location in (1, N)
+        -- N : total length of the sequence
+        -- cp_i : all change locations detected by Binary segmentation
     """
     test_stat_i = {}
-    cp_i = list_of_cp[1:-1]
-    for each_cp in range(len(cp_i)):
-        prev_cp= list_of_cp[list_of_cp.index(cp_i[each_cp])-1]
-        next_cp = list_of_cp[list_of_cp.index(cp_i[each_cp])+1]
-        datamatrix = fdata[ prev_cp: next_cp]    #takes care of calculating the statistic between prev. and next. change points
-        N = len(datamatrix[:, 0])
-        _, test_stat_i[str(cp_i[each_cp])] = cusum(datamatrix, begin=0, end=N, N=N, thresh_val=binseg_thresh)   # value of the test statistic at the supremum
-    return test_stat_i
+    for cp_index in range(1, len(cp_i)-1):
+        begin = cp_i[cp_index-1]
+        end = cp_i[cp_index +1]
+        dataPoints = end - begin
+        sValues = np.linspace(float(begin)/float(N), float(end - 1)/float(N), num=dataPoints)
+        M_hat = []
+        for spoints_i in range(dataPoints):  # scanning over possible values of s
+            U = U_hat(sValues[spoints_i], data_mat, begin, end, N)
+            hat_M =  np.max(np.abs(U))  #sup over t for fixed s
+            M_hat.append(hat_M) # store this for all s
+
+        #plt.plot(M_hat)
+        #plt.vlines(x = np.argmax(M_hat), ymin=0, ymax=20)
+        #plt.show()
+        # print(M_hat[cp_i[cp_index]-begin])
+        test_stat_i[str(cp_i[cp_index])] = M_hat[cp_i[cp_index]-begin]#*dataPoints
+    return  test_stat_i
+
+
+
+
 
 def mean_funcs(func_data, est_changepoint):
     """
@@ -109,6 +123,7 @@ def mean_funcs(func_data, est_changepoint):
     """
     mu_diff = []
     cp_consideration = est_changepoint[1:-1]   # removing end points i.e., 0 and n
+
     for cp in cp_consideration:
         cp_m1 = est_changepoint[est_changepoint.index(cp)-1]  #prev cp
         cp_p1 = est_changepoint[est_changepoint.index(cp)+1]  #next cp
@@ -132,7 +147,6 @@ def extremal_points(mu_diff, c, n, M_hat, est_cp):
     E_upper = []
     E_lower = []
     d_hat = []   # store d_hat corresponding to each CP.
-
     for cp in range(1,len(est_cp)-1):
         d_hat_i = M_hat[str(est_cp[cp])]   # Test statistic corresponding to CP
         upper_thresh = d_hat_i - c_thresh/np.sqrt(n)  # RHS E_plus
@@ -155,34 +169,60 @@ def bootstrap_func(func_data, est_cp, mu_diff, E_upper, E_lower, l, M=2):
     N = len(func_data[:, 0])
     T_max = []
     ni = []
-    s_tild_hat = []
+    h_hat = []
     sValues = np.linspace(0, 1, num = N)
+    fdata = np.copy(func_data)  
+    
     for i in range(1, len(est_cp)-1): 
-        cp = est_cp[i]
-        cp_m1= est_cp[i-1]
-        cp_p1= est_cp[i+1]
+        fdata = np.copy(func_data)
+        #----------locations of current, previous and next cp -------#
+        cp = est_cp[i]   # current cp
+        cp_m1= est_cp[i-1] # previous cp
+        cp_p1= est_cp[i+1] # next cp
+        #-----------extremal sets corresponding to the cp -----------#
         E_plus, E_minus = E_upper[i-1], E_lower[i-1]    
         s_hat = sValues[int(cp)]                      #  Estimated change-point between 0, 1
-        s_tilde =  (s_hat- sValues[int(cp_m1)])/(sValues[int(cp_p1)-1]- sValues[int(cp_m1)]) 
-        fdata = func_data[cp_m1: cp_p1, :]  # slicing the data pre and post cp
-        s_tild_hat.append(s_tilde)
-        n_i = est_cp[i+1]- est_cp[i-1]
-        ni.append(n_i)
-        fdata[cp- cp_m1:, :] = fdata[cp-cp_m1:, :]- mu_diff[i-1]        # Y_j centering data after est. change-point with mu_diff,  check immediately after eq. 4.15
+        h_hat_s =  (s_hat- sValues[int(cp_m1)])/(sValues[int(cp_p1)-1]- sValues[int(cp_m1)])    # rescaling for the correct interval
+        n_i = cp_p1- cp_m1
+        ni.append(n_i), h_hat.append(h_hat_s)
+        #----------adjusting the data for respective cp -------------#
+        fdata = fdata[cp_m1: cp_p1, :]
+        mu_diff[i-1] = mu_diff[i-1].reshape((1, 100))
+        fdata[cp- cp_m1:, :] -= mu_diff[i-1]
         rv = np.random.normal(0, 1, N*M)
-        epsilon_hat = fdata - np.mean(fdata, axis = 0)   # first term from paper
+        epsilon_hat = fdata - l*np.mean(fdata, axis = 0)   # first term from paper from the bootstrap process
         epsilon_star = []
         """ l is bandwidth parameter, rv is multiplier random variable ! """
         for k in range(1, n_i-l+1):
             quant  = (1/np.sqrt(l))*np.sum(epsilon_hat[k: (k+l), :], axis = 0)   # second factor need not be included... 
-            quant = quant*rv[k]
+            quant = quant*rv[k]    
             epsilon_star.append(quant)      # sequentially appending from for loop
-        epsilon_star = np.asarray(epsilon_star)
 
-        B_1 = np.multiply((1/np.sqrt(n_i)), np.sum(epsilon_star, axis = 0))
-        B_s = np.multiply((1/np.sqrt(n_i)), np.sum(epsilon_star[int(cp_m1):int(cp)], axis = 0))
-        W_hat = B_s - np.multiply((s_tilde), B_1)    #(1/n_i)*        # quantity for bootstrap repetitions ! # stilde here
+        epsilon_star = np.asarray(epsilon_star)
+        B_1 = np.multiply((1/np.sqrt(n_i)), np.sum(epsilon_star, axis = 0))   # full segment under considerations
+        B_s = np.multiply((1/np.sqrt(n_i)), np.sum(epsilon_star[:int(cp)], axis = 0))
+
+        W_hat = B_s - np.multiply((h_hat_s), B_1)#*(1/n_i)        # quantity for bootstrap repetitions ! # stilde here
         
+        """
+        #print(int(h_hat_s*N))
+        #W_hat_rescaled = np.multiply((1/np.sqrt(n_i)), np.sum(epsilon_star[:int(h_hat_s*N)], axis = 0))-np.multiply((h_hat_s), B_1)
+
+        if len(E_plus) > 0:    
+            T_plus =  np.max(W_hat_rescaled[E_plus])  # sup over each extremal set
+        else:
+            T_plus = float("-inf")
+        
+        if len(E_minus) > 0:
+            T_minus = np.max(-W_hat_rescaled[E_minus])
+        else:
+            T_minus = float("-inf")
+        
+        W_factor_test = max(T_plus, T_minus)            # max over the two different extremals...!
+        T = W_factor_test#*(1/s_hat*(1-s_hat))    # rescaling wrong and from previous paper.... 
+        T_max.append(T)  # over which max_i will be taken    
+
+        """
         if len(E_plus) > 0:    
             T_plus =  np.max(W_hat[E_plus])  # sup over each extremal set
         else:
@@ -196,7 +236,7 @@ def bootstrap_func(func_data, est_cp, mu_diff, E_upper, E_lower, l, M=2):
         W_factor_test = max(T_plus, T_minus)            # max over the two different extremals...!
         T = W_factor_test#*(1/s_hat*(1-s_hat))    # rescaling wrong and from previous paper.... 
         T_max.append(T)  # over which max_i will be taken   
-    
+        
     if len(T_max) > 0:
         max_value = max(T_max)
         # Use the maximum value as needed
@@ -204,7 +244,7 @@ def bootstrap_func(func_data, est_cp, mu_diff, E_upper, E_lower, l, M=2):
     # Handle the case when the sequence is empty
         max_value = 1000000
     
-    return max_value, ni, s_tild_hat
+    return max_value, ni, h_hat
 
 
 
@@ -224,30 +264,34 @@ def reject(data_mat, change_points,  Delta, level_alpha, repeats, const_c, binse
         --
         --
     """
+
     # Calculate or use user-defined binseg_thresh
     if binseg_thresh is None:
         binseg_thresh = calculate_median_l2_norm_squared(data_mat)
+
     if block_length_boot is None:
         block_length_boot = w_funcs_q(h_val = 9, timeseries=data_mat, weight_function='qs')
+        
 
     N = len(data_mat[:,0])     # number of samples of functional data 
     T_star = []
     mu_diff = mean_funcs(data_mat, change_points) #  mean difference before and after change locations
-    test_stat =  mult_test_stat(change_points, data_mat, binseg_thresh)   # Test statistic at each change location
+    test_stat =  mult_test_stat(data_mat, N, change_points)   # Test statistic at each change location
     E_upper, E_lower, _ = extremal_points(mu_diff, const_c, N, test_stat, change_points)  # E_plus and E_minus (the extremal points)
+
     for _ in range(repeats): 
         data_mat_copy = np.copy(data_mat)
-        T_st, n_i, s_Tilde= bootstrap_func(data_mat_copy, change_points, mu_diff, E_upper, E_lower, block_length_boot, M)
+        T_st, n_i, h_hat= bootstrap_func(data_mat_copy, change_points, mu_diff, E_upper, E_lower, block_length_boot, M)
         T_star.append(T_st)   #value of bootstrap samples
 
-    quantile = np.quantile(np.asarray(T_star), 1-level_alpha)
-    s_tilde_fact = np.multiply(np.array(s_Tilde), (1- np.array(s_Tilde)))
+    quantile = np.quantile(T_star, 1-level_alpha)
+    h_hat_all = np.multiply(np.array(h_hat), (1- np.array(h_hat)))
     teststat_list = list(test_stat.values())
     test_dec = {}
     for delta in Delta:
-        rhs_1 = np.array(teststat_list)- np.multiply(s_tilde_fact, delta)     # coord-wise multiply
+        rhs_1 = np.array(teststat_list)- np.multiply(h_hat_all, delta)     # coord-wise multiply
         rhs = np.multiply(np.sqrt(n_i), rhs_1)
-        #print(max(rhs), quantile)
+        print(max(rhs), quantile)
         global_rej = ((max(rhs))>= quantile)*1
         rej = (rhs>= quantile)
         rej = rej*1
@@ -327,7 +371,7 @@ def compute_c_0(w_corrs, weight_func, q, N):
     norm_Chat_2te =  ((np.linalg.norm(w_corrs)**2 + np.trace(w_corrs)**2) * np.linalg.norm(weight_func)**2)**(-1/(1+2*q))
     c_0 = norm_Chat_1st*norm_Chat_2te
     opt_h = c_0*(N**(1/(1+2*q)))
-    return opt_h
+    return int(opt_h)
 
 
 
@@ -375,18 +419,18 @@ def w_funcs_q(h_val, timeseries, weight_function='qs'):
 
 
 
-
-
 def calculate_squared_l2_norm(data):
     differences = np.diff(data, axis=0)
-    squared_l2_norms = np.sum(differences**2, axis=0)#/2
+    squared_l2_norms = np.sum(differences**2, axis=0)/2
     return squared_l2_norms
 
 def calculate_median_l2_norm_squared(data):
+    n = len(data[:, 0])
     squared_l2_norms = calculate_squared_l2_norm(data)
     sigma_n_2 = np.median(squared_l2_norms)
-    sigma_n = sigma_n_2#**(1/2)
-    return sigma_n
+    sigma_n = sigma_n_2**(1/2)
+    thresh = sigma_n*np.sqrt(3*np.log(n))
+    return thresh
 
 #---------------------------------------------------------------------------------------------------#
 #-               Ancillary functions III: generate data for testing the method                     -#
@@ -411,3 +455,7 @@ def gen_data(num_curves, change_locs, time_points):
         elif len(change_locs) > 2 and idx >= change_locs[2]:
             noisy_curves[idx:, 0:16] -= [2, 5, 9, 10, 12, 15, 22, 25, 25, 22, 15, 12, 10, 9, 5, 2]
     return noisy_curves
+
+
+
+
--- data_example.py
+++ data_example.py
@@ -9,11 +9,11 @@ import pandas as pd
 delta_size = 1
 alpha = 0.1
 boot_repeats = 100
-bin_seg_thresh =1
+bin_seg_thresh =7
 #import datamat directly
 datamat = pd.read_csv('/Users/rupsabasu/Documents/DATASETS/dataMat/indoor_fatigue/RightKnee/3.csv') #3 #2
 datavals = datamat.values
 
-segs, M_hat, cusumstat = bfmcpp.binary_seg_init(datavals, bin_seg_thresh)
-relevant_cp, global_rej = bfmcpp.reject(datavals, segs, Delta= [delta_size], level_alpha=alpha, repeats=boot_repeats, const_c= 0.01)
-detected_cp = segs[1:]
+segs = bfmcpp.binary_seg_init(datavals, bin_seg_thresh)
+test_reject = bfmcpp.reject(datavals, segs, [delta_size], alpha, boot_repeats, 0.01 )
+print(test_reject)
