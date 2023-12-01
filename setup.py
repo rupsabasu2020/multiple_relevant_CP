676d81bfc6ba6e80f8f97144e1c2052fde47e299
rupsabasu2020 <rupsa.basu2020@gmail.com>
2023-12-01 10:50:56 +0100

    testpyPi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(ROOT, 'README.md'), encoding="utf-8") as f:
 # name is not fixed yet
 # version 0 (i.e. not ready yet)
 setup(name='multiple_relevant_CP',
-      version='0.0.1',
+      version='0.0.2',
       description='A package to detect relevant changes.',
      author='Rupsa Basu',
       author_email='r.basu@utwente.nl',
@@ -19,10 +19,8 @@ setup(name='multiple_relevant_CP',
       include_package_data=True,
       install_requires=[
           'numpy',
-          'ortools>=9.4',
           'scipy',
           'matplotlib',
-           'matplotlib_scalebar',
            'scipy',
            'pandas',
            'multiprocessing',
